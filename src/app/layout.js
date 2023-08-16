19fc4a5b7aa6d1e11715f82fbde7a0e4bb70d928
Rahul <rahulrawattrippy@gmail.com>
2023-08-16 11:43:21 +0530

    update meta title

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,43 +14,37 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" type="image/x-icon" href="/logo.svg" />
-        <link
-          rel="canonical"
-          href="https://www.telepartywatchparty.com/"
-        />
+        <link rel="canonical" href="https://www.teleparty.pro/" />
 
-        <meta
-          property="og:url"
-          content="https://www.telepartywatchparty.com/"
-        />
+        <meta property="og:url" content="https://www.teleparty.pro/" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Disney plus watch party " />
+        <meta
+          property="og:title"
+          content=" Watch together with teleparty extension "
+        />
         <meta
           property="og:description"
           content="Unite with friends and family on virtual movie night with teleparty extension and create a special memory. "
         />
         <meta
           property="og:image"
-          content="https://www.telepartywatchparty.com/main-logo.svg"
+          content="https://www.teleparty.pro/main-logo.svg"
         />
 
         <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="teleparty.pro" />
+        <meta property="twitter:url" content="https://www.teleparty.pro/" />
         <meta
-          property="twitter:domain"
-          content="telepartywatchparty.com"
+          name="twitter:title"
+          content=" Watch together with teleparty extension "
         />
-        <meta
-          property="twitter:url"
-          content="https://www.telepartywatchparty.com/"
-        />
-        <meta name="twitter:title" content="Disney plus watch party " />
         <meta
           name="twitter:description"
           content="Unite with friends and family on virtual movie night with teleparty extension and create a special memory. "
         />
         <meta
           name="twitter:image"
-          content="https://www.telepartywatchparty.com/main-logo.svg"
+          content="https://www.teleparty.pro/main-logo.svg"
         />
       </head>
       <body>
