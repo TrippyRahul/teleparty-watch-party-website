3342952a6b7bf152c15789aaa71a52e732447326
Rahul <rahulrawattrippy@gmail.com>
2023-09-13 16:47:39 +0530

    fix meta desc

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -10,27 +10,6 @@ const blogsData = [
     date: "Sep 13, 2023",
     slug: "teleparty-extension-vs-scener-chrome-extension",
   },
-  {
-    id: "1",
-    title: "Teleparty extension Vs Scener Chrome extension",
-    desc: "Planning to watch a movie with your family and friends, but somehow you can not make it due to some reasons. Worry not! You can still stream your favorite shows with them. As there are multiple Ott platforms that provide unlimited movies and shows. Scener and Teleparty extension are great options where you can plan your movie date with your special one. They both have great features and offer a vast variety of the latest TV shows, series, and movies. ",
-    date: "Sep 13, 2023",
-    slug: "teleparty-extension-vs-scener-chrome-extension",
-  },
-  {
-    id: "1",
-    title: "Teleparty extension Vs Scener Chrome extension",
-    desc: "Planning to watch a movie with your family and friends, but somehow you can not make it due to some reasons. Worry not! You can still stream your favorite shows with them. As there are multiple Ott platforms that provide unlimited movies and shows. Scener and Teleparty extension are great options where you can plan your movie date with your special one. They both have great features and offer a vast variety of the latest TV shows, series, and movies. ",
-    date: "Sep 13, 2023",
-    slug: "teleparty-extension-vs-scener-chrome-extension",
-  },
-  {
-    id: "1",
-    title: "Teleparty extension Vs Scener Chrome extension",
-    desc: "Planning to watch a movie with your family and friends, but somehow you can not make it due to some reasons. Worry not! You can still stream your favorite shows with them. As there are multiple Ott platforms that provide unlimited movies and shows. Scener and Teleparty extension are great options where you can plan your movie date with your special one. They both have great features and offer a vast variety of the latest TV shows, series, and movies. ",
-    date: "Sep 13, 2023",
-    slug: "teleparty-extension-vs-scener-chrome-extension",
-  },
 ];
 
 const page = () => {
