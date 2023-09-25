3349ce75060cf52918cf8260307566d4c987ce91
TrippyGaurav <gaurav.trippybug@gmail.com>
2023-09-25 15:26:00 +0530

    update button link

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -4,10 +4,11 @@ import styles from "../styles/button.module.scss";
 
 const Button = () => {
   const handleButtonClick = () => {
-      const redirectURL = "https://chrome.google.com/webstore/detail/watchottparty/akbbdmilignoplkhminiaecccigmpdmo?hl=en-US";
-      window.open(redirectURL, "_blank");
+    const redirectURL =
+      "https://chrome.google.com/webstore/detail/teleparty-pro/gfmbbcakoflhbbamcfcfgclgjibaodgc/related?hl=en";
+    window.open(redirectURL, "_blank");
   };
-  
+
   return (
     <button className={styles.btn} onClick={handleButtonClick}>
       Add to Chrome
