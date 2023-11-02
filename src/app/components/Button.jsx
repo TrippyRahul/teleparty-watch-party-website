8a7a2f74269b65aefa1613b98299ae9947b08df6
TrippyGaurav <gaurav.trippybug@gmail.com>
2023-11-02 15:21:25 +0530

    fix button

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -1,21 +1,13 @@
-"use client";
 import React from "react";
 import styles from "../styles/button.module.scss";
 
 const Button = () => {
-  const handleButtonClick = () => {
-    const redirectURL = "";
-    window.open(redirectURL, "_blank");
-  };
-
   return (
     <a
       href="https://chrome.google.com/webstore/detail/teleparty-pro/gfmbbcakoflhbbamcfcfgclgjibaodgc/related?hl=en"
       target="_blank"
     >
-      <button className={styles.btn} onClick={handleButtonClick}>
-        Add to Chrome
-      </button>
+      <button className={styles.btn}>Add to Chrome</button>
     </a>
   );
 };
